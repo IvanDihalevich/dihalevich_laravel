e815edc201da22dbcc92e04e1cb7d258a927a921
Ivan Dihalevych <143941559+IvanDihalevich@users.noreply.github.com>
2024-06-19 00:11:24 +0300

    feat: lab 11 done

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RestTestController;
 use App\Http\Controllers\Blog\PostController; // Import PostController
+use App\Http\Controllers\DiggingDeeperController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -21,6 +22,14 @@ Route::middleware([
 // Route group for blog posts with namespace and prefix
 Route::group(['namespace' => 'App\Http\Controllers\Blog', 'prefix' => 'blog'], function () {
     Route::resource('posts', PostController::class)->names('blog.posts');
+});
+
+Route::group(['prefix' => 'digging_deeper'], function () {
+
+    Route::get('collections', [DiggingDeeperController::class, 'collections'])
+
+        ->name('digging_deeper.collections');
+
 });
 
 // Resource route for RestTestController
